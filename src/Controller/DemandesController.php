3d12559cc27285700a16eb6a8691758c2c557952
Helmi-Simplon <shan.tunisois@gmail.com>
2021-07-16 16:35:13 +0200

    Ajout de la page détail d'une demande

--- src/Controller/DemandesController.php
+++ src/Controller/DemandesController.php
@@ -4,8 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Demandes;
 use App\Form\DemandesType;
-use App\Repository\UserRepository;
-use App\Repository\AdresseRepository;
 use App\Repository\DemandesRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -33,6 +31,24 @@ class DemandesController extends AbstractController
 
 
 /**
+ * @Route("/vendeurs/demande/{slug}", name="demande_detail", methods={"GET"})
+ */
+public function detailDemande(Demandes $demande): Response
+    {
+        //dd($demande);
+    return $this->render('demandes/demande_detail.html.twig', [
+        'controller_name' => 'TestController',
+        'demande' => $demande,
+    ]);
+    }
+
+
+
+
+
+
+
+/**
      * @Route("/acheteurs/add", name="demande_add")
      */
     public function addDemande(Request $request): Response
@@ -42,7 +58,7 @@ class DemandesController extends AbstractController
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            
+
             $demande->setAcheteur($this->getUser());
             $em = $this->getDoctrine()->getManager();
             $em->persist($demande);
