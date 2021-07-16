c6990e9d2ba69bd6ab7b7970872752bd7dd0f1ba
Helmi-Simplon <shan.tunisois@gmail.com>
2021-07-16 13:37:33 +0200

    ajouter photo de profil sur les pages quand l'utilisateur est connecté + changer l'arrière plan des cartes dans la page demandes

--- src/Controller/DemandesController.php
+++ src/Controller/DemandesController.php
@@ -37,25 +37,16 @@ class DemandesController extends AbstractController
      */
     public function addDemande(Request $request): Response
     {
-        //On crée un objet $demande à partir de la classe Demandes(App\Entity\Demandes)
         $demande = new Demandes();
-        //On crée un formulaire en utilisant la méthode createForm() avec les paramètres suivants : La classe DemandesType
-        // et l'objet $demande
         $form = $this->createForm(DemandesType::class, $demande);
-        //On appelle la methode handleRequest() qui prend $request comme paramètre, il vérifie les données envoyées 
-        //quand le formulaire est validé
+
         $form->handleRequest($request);
-        //si le formulaire est validé et les données sont valides
         if ($form->isSubmitted() && $form->isValid()) {
-            //On parmètre le champs user avec le user fournit dans le formulaire
+            
             $demande->setAcheteur($this->getUser());
-            //on appelle la methode getManager à partir de l'entité manager pour manager les requetes à utiliser
             $em = $this->getDoctrine()->getManager();
-            //on fait un persist pour entrer le resultat de la requête utiliser dans la base
             $em->persist($demande);
-            //on committe avec un flush
             $em->flush();
-            //on se redirige vers la page d'accueil après la fin de l'opération
             return $this->redirectToRoute('demandes');
         }
 
