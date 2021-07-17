bea46b0df6dc998f252e408f069f2aec03ecd6b0
Helmi-Simplon <shan.tunisois@gmail.com>
2021-07-17 14:00:56 +0200

    création d'un formulaire de création d'offre via le make:form + création de la méthode addOffres() dans le controller Offres

--- src/Controller/OffresController.php
+++ src/Controller/OffresController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Offres;
 use App\Repository\OffresRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class OffresController extends AbstractController
 {
@@ -22,5 +24,26 @@ class OffresController extends AbstractController
         ]);
     }
 
-    
+    /**
+     * @Route("/vendeurs/add", name="offre_add")
+     */
+    public function addOffres(Request $request): Response
+    {
+        $offres = new Offres();
+        $form = $this->createForm(OffresType::class, $offres);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $offres->setVendeur($this->getUser());
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($offres);
+            $em->flush();
+            return $this->redirectToRoute('offres');
+        }
+
+        return $this->render('offres/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 }
