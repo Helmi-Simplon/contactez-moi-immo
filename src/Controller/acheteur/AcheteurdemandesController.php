2ce82ce16ecc646c6ab5311350355efda5ffca0c
Helmi-Simplon <shan.tunisois@gmail.com>
2021-07-25 04:12:44 +0200

    maj controlleur AcheteurdemandesController.php en ajoutant une root pour deposer les demandes de l'acheteur dans son espace + création de la vue add.html.twig

--- src/Controller/acheteur/AcheteurdemandesController.php
+++ src/Controller/acheteur/AcheteurdemandesController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\acheteur;
 
 use App\Entity\Demandes;
+use App\Form\DemandesType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,4 +22,29 @@ class AcheteurdemandesController extends AbstractController
            
         ]);
     }
+
+     /**
+     * @Route("/acheteurdemandes/add", name="acheteurdemandes_add")
+     */
+    public function ajoutDemande(Request $request): Response
+    {
+        $demande = new Demandes();
+        $form = $this->createForm(DemandesType::class, $demande);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $demande->setAcheteur($this->getUser());
+            $demande->setActif(true);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($demande);
+            $em->flush();
+            return $this->redirectToRoute('demandes');
+        }
+
+        return $this->render('acheteurBO/acheteurdemandes/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
 }
