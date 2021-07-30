4e797dc1237d819dace877cfc47be454e2d79f26
Helmi-Simplon <shan.tunisois@gmail.com>
2021-07-30 17:30:13 +0200

    correction bug affichage

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
+use App\Form\ContactType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -39,7 +40,7 @@ class PagesController extends AbstractController
             $em->flush();
             $this->addFlash('success','Votre courriel a été envoyé, un retour sera envoyé à ' . $contact->getExpediteur());
 
-            return $this->redirectToRoute('home');
+            return $this->redirectToRoute('home_contact');
         }
         return $this->render('pages/contact.html.twig', [
             'form' => $form->createView(),
